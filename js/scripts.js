4aa61a6e7e1d6e95d0fa2d6b9c44695a4c6e63d6
cBrain <dylanbiddle@gmail.com>
2021-08-27 01:23:45 -0400

    fix map and add new service

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,18 @@ function initMap() {
     const richmond = { lat: 37.74, lng: -84.29 };
     // The map, centered at Richmond
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
+        zoom: 8,
         center: richmond,
     });
-    // The marker, positioned at Richmond
-    const marker = new google.maps.Marker({
-        position: richmond,
+    // The circle, positioned at Richmond
+    const circle = new google.maps.Circle({
+        center: richmond,
         map: map,
+        radius: 1000 * 100,
+        strokeColor: "#FCDF02",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FFFFFF",
+        fillOpacity: 0.35
     });
 }
